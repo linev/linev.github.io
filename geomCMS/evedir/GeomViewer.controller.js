53e43ddd82a3c1a6e4d225495caf6f85a84045b9
Sergey Linev <S.Linev@gsi.de>
2019-01-09 09:44:44 +0100

    Introduce eve.ColorBox to draw color box in TreeTable

--- geomCMS/evedir/GeomViewer.controller.js
+++ geomCMS/evedir/GeomViewer.controller.js
@@ -5,7 +5,54 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
                'sap/ui/layout/SplitterLayoutData',
                "sap/ui/core/ResizeHandler"
 ],function(Controller, CoreControl, JSONModel, Splitter, SplitterLayoutData, ResizeHandler) {
+
    "use strict";
+   
+   CoreControl.extend("eve.ColorBox", { // call the new Control type "my.ColorBox" and let it inherit from sap.ui.core.Control
+
+      // the control API:
+      metadata : {
+         properties : {           // setter and getter are created behind the scenes, incl. data binding and type validation
+            "color" : {type: "sap.ui.core.CSSColor", defaultValue: "#fff"} // you can give a default value and more
+         }
+      },
+
+      // the part creating the HTML:
+      renderer : function(oRm, oControl) { // static function, so use the given "oControl" instance instead of "this" in the renderer function
+         if (oControl.getColor() == "lightblue") return;
+         
+         oRm.write("<div"); 
+         oRm.writeControlData(oControl);  // writes the Control ID and enables event handling - important!
+         oRm.addStyle("background-color", oControl.getColor());  // write the color property; UI5 has validated it to be a valid CSS color
+         oRm.writeStyles();
+         oRm.addClass("eveColorBox");      // add a CSS class for styles common to all control instances
+         oRm.writeClasses();              // this call writes the above class plus enables support for Square.addStyleClass(...)
+         oRm.write(">"); 
+         oRm.write("</div>"); // no text content to render; close the tag
+      },
+
+      // an event handler:
+      onclick : function(evt) {   // is called when the Control's area is clicked - no further event registration required
+         sap.ui.require([
+            'sap/ui/unified/ColorPickerPopover'
+         ], function (ColorPickerPopover) {
+            if (!this.oColorPickerPopover) {
+               this.oColorPickerPopover = new ColorPickerPopover({
+                  change: this.handleChange.bind(this)
+               });
+            }
+            this.oColorPickerPopover.setColorString(this.getColor());
+            this.oColorPickerPopover.openBy(this);
+         }.bind(this));
+      },
+
+      handleChange: function (oEvent) {
+         var newColor = oEvent.getParameter("colorString");
+         this.setColor(newColor);
+         // TODO: fire a "change" event, in case the application needs to react explicitly when the color has changed
+         // but when the color is bound via data binding, it will be updated also without this event
+      }
+   });
    
    CoreControl.extend("eve.GeomDraw", { 
 
@@ -49,7 +96,7 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
             this.geo_painter.assignClones(this.geo_clones);
          
          if (this.geo_visisble) {
-            this.geo_painter.prepareObjectDraw(this.geo_visisble,"__geom_viewer_selection__");
+            this.geo_painter.prepareObjectDraw(this.geo_visisble, "__geom_viewer_selection__");
             delete this.geo_visisble;
          }
       },
@@ -66,7 +113,7 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
       
       startDrawing: function(visible) {
          if (this.geo_painter)
-            this.geo_painter.prepareObjectDraw(visible,"__geom_viewer_selection__");
+            this.geo_painter.prepareObjectDraw(visible, "__geom_viewer_selection__");
          else
             this.geo_visisble = visible;
       },
@@ -104,8 +151,21 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
          
          this.creator = new JSROOT.EVE.EveElements();
 
+         
+         var t = this.getView().byId("treeTable");
+         
+         t.addColumn(new sap.ui.table.Column({
+            label: "Description",
+            template: new sap.ui.layout.HorizontalLayout({
+               content: [
+                  new eve.ColorBox({color:"{color}" }),
+                  new sap.m.Text({text:"{title}", wrapping: false })
+               ]
+            })
+          }));
+         
          // catch re-rendering of the table to assign handlers 
-         this.getView().byId("treeTable").addEventDelegate({
+         t.addEventDelegate({
             onAfterRendering: function() { this.assignRowHandlers(); }
          }, this);
          
@@ -308,10 +368,13 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
 
          var node = this.clones.nodes[indx];
          
-         this.tree_nodes[indx] = tnode = { title: node.name, id: indx };
+         this.tree_nodes[indx] = tnode = { title: node.name, id: indx, color: "green" };
          
-         if (node.chlds) {
+         // if (indx<10) tnode.color = "lightblue";
+         
+         if (node.chlds && (node.chlds.length>0)) {
             tnode.chlds = [];
+            tnode.color = "lightblue";
             for (var k=0;k<node.chlds.length;++k) 
                tnode.chlds.push(this.buildTreeNode(node.chlds[k]));
          }
@@ -622,4 +685,5 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
          }
       }
    });
+   
 });
