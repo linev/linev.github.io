fbc7f3a8a0e73e2920c6440f25a9e7475d20f27c
Sergey Linev <S.Linev@gsi.de>
2019-01-24 14:45:28 -0800

    Enable shift key handling

--- eve7/evedir/EveScene.js
+++ eve7/evedir/EveScene.js
@@ -14,9 +14,9 @@
 } (function(JSROOT) {
 
    "use strict";
-   
+
    /// constructor, handle for REveScene class
-   
+
    function EveScene(mgr,scene,viewer)
    {
       this.mgr = mgr;
@@ -27,20 +27,20 @@
       this.creator.useIndexAsIs = (JSROOT.GetUrlOption('useindx') !== null);
       this.id2obj_map = {}; // base on element id
       this.mid2obj_map = {}; // base on master id
-      
+
       this.first_time = true;
-      
+
       this.selected = {}; // generic map of selected objects
-      
+
       // register ourself for scene events
        this.mgr.RegisterSceneReceiver(scene.fSceneId, this);
    }
-   
+
    EveScene.prototype.hasRenderData = function(elem)
    {
-      if (elem===undefined)          
+      if (elem===undefined)
          elem = this.mgr.GetElement(this.id);
-      
+
       if (!elem) return false;
       if (elem.render_data) return true;
       if (elem.childs)
@@ -48,7 +48,7 @@
             if (this.hasRenderData(elem.childs[k])) return true;
       return false;
    }
-   
+
    EveScene.prototype.makeGLRepresentation = function(elem)
    {
       if (!elem.render_data) return null;
@@ -63,30 +63,30 @@
          obj3d.geo_name = elem.fName; // used for highlight
 
          obj3d.scene = this; // required for get changes when highlight/selection is changed
-         
+
          //AMT: reference needed in MIR callback
          obj3d.eveId = elem.fElementId;
          obj3d.mstrId = elem.fMasterId;
-         
+
          if (elem.render_data.matrix) {
             obj3d.matrixAutoUpdate = false;
             obj3d.matrix.fromArray( elem.render_data.matrix );
             obj3d.updateMatrixWorld(true);
          }
-         
+
          return obj3d;
       }
    }
-   
+
    EveScene.prototype.create3DObjects = function(all_ancestor_children_visible, prnt, res3d)
    {
-      if (prnt === undefined) {          
+      if (prnt === undefined) {
          prnt = this.mgr.GetElement(this.id);
          res3d = [];
       }
 
       if (!prnt || !prnt.childs) return res3d;
-      
+
       for (var k = 0; k < prnt.childs.length; ++k)
       {
          var elem = prnt.childs[k];
@@ -107,7 +107,7 @@
                   // Object3D to separate "graphical" children and structural children.
 
                   res3d.push(obj3d);
-                  
+
                   this.id2obj_map[elem.fElementId] = obj3d;
                   if (elem.fMasterId) this.mid2obj_map[elem.fMasterId] = obj3d;
 
@@ -119,29 +119,29 @@
 
          this.create3DObjects(elem.fRnrChildren && all_ancestor_children_visible, elem, res3d);
       }
-      
+
       return res3d;
-   } 
-   
+   }
+
    /** method insert all objects into three.js container */
    EveScene.prototype.redrawScene = function()
    {
       if (!this.viewer) return;
-      
+
       var res3d = this.create3DObjects(true);
       if (!res3d.length && this.first_time) return;
-      
+
       var cont = this.viewer.getThreejsContainer("scene" + this.id);
       while (cont.children.length > 0)
          cont.remove(cont.children[0]);
-      
+
       for (var k = 0; k < res3d.length; ++k)
          cont.add(res3d[k]);
-      
+
       this.viewer.render();
       this.first_time = false;
    }
-   
+
    EveScene.prototype.getObj3D = function(elementId, is_master)
    {
       var map = is_master ? this.mid2obj_map : this.id2obj_map;
@@ -167,10 +167,10 @@
 
          this.update3DObjectsVisibility(elem.childs, elem.fRnrChildren && all_ancestor_children_visible);
       }
-   }   
+   }
 
 
-   
+
    EveScene.prototype.colorChanged = function(el)
    {
       console.log("color change ", el.fElementId, el.fMainColor);
@@ -181,10 +181,10 @@
    EveScene.prototype.replaceElement = function(el)
    {
       if (!this.viewer) return;
-      
+
       var obj3d = this.getObj3D(el.fElementId);
       var all_ancestor_children_visible = obj3d.all_ancestor_children_visible;
-      
+
       var container = this.viewer.getThreejsContainer("scene" + this.id);
 
       container.remove(obj3d);
@@ -193,30 +193,30 @@
       obj3d.all_ancestor_children_visible = obj3d;
 
       container.add(obj3d);
-      
+
 
       this.id2obj_map[el.fElementId] = obj3d;
       if (el.fMasterId) this.mid2obj_map[el.fMasterId] = obj3d;
-      
+
       this.viewer.render();
    }
-   
+
    EveScene.prototype.elementAdded = function(el) {
       if (!this.viewer) return;
-      
+
       var obj3d =  this.makeGLRepresentation(el);
       if (!obj3d) return;
 
-      // AMT this is an overkill, temporary solution      
+      // AMT this is an overkill, temporary solution
       var scene = this.mgr.GetElement(el.fSceneId);
       this.update3DObjectsVisibility(scene.childs, true);
-      
+
       var container = this.viewer.getThreejsContainer("scene" + this.id);
 
       container.add(obj3d);
       console.log("element added ", el);
    }
-   
+
    EveScene.prototype.visibilityChanged = function(el)
    {
       var obj3d = this.getObj3D( el.fElementId );
@@ -243,16 +243,16 @@
          this.update3DObjectsVisibility(scene.childs, true);
       }
    }
-   
+
    /** interactive handler. Calculates selection state, apply to element and distribute to other scene */
    EveScene.prototype.processElementSelected = function(obj3d, col, indx, evnt) {
-      // first decide if element selected or not 
-      
+      // first decide if element selected or not
+
       var id = obj3d.mstrId,
           sel = this.selected[id];
-      
+
       if (indx === undefined) {
-         if (!sel) { 
+         if (!sel) {
             sel = this.selected[id] = { id: id, col: col };
          } else {
             // means element selected, one should toggle back
@@ -260,19 +260,30 @@
             delete this.selected[id];
          }
       } else {
-         if (!sel) 
+         if (!sel)
             sel = this.selected[id] = { id: id, col: col, indx: [] };
          if (evnt && evnt.ctrlKey) {
             var pos = sel.indx.indexOf(indx);
             if (pos < 0) sel.indx.push(indx); else
                          sel.indx.splice(pos, 1);
-         } else { 
+         } else if (evnt && evnt.shiftKey) {
+            if (sel.indx.length != 1) {
+               sel.indx = [ indx ];
+            } else {
+               var min = Math.min(sel.indx[0], indx),
+                   max = Math.max(sel.indx[0], indx);
+               sel.indx = [];
+               if (min != max)
+                  for (var i=min;i<=max;++i)
+                     sel.indx.push(i);
+            }
+         } else {
             if ((sel.indx.length == 1) && (sel.indx[0] == indx))
                sel.indx = [];
             else
                sel.indx = [ indx ];
          }
-      
+
          if (!sel.indx.length) {
             sel.col = null;
             sel.indx = undefined;
@@ -283,16 +294,16 @@
       this.setElementSelected(id, sel.col, sel.indx, true);
 
       this.mgr.invokeInOtherScenes(this, "setElementSelected", id, sel.col, sel.indx);
-      
+
       // when true returns, controller will not try to render itself
       return true;
    }
-   
+
    /** interactive handler */
    EveScene.prototype.processElementHighlighted = function(obj3d, col, indx, evnt) {
       var id = obj3d.mstrId;
       // id = obj3d.eveId;
-      
+
       this.setElementHighlighted(id, col, indx, true);
 
       this.mgr.invokeInOtherScenes(this, "setElementHighlighted", id, col, indx);
@@ -300,43 +311,43 @@
       // when true returns, controller will not try to render itself
       return true;
    }
-   
+
    /** returns true if highlight index is differs from current */
    EveScene.prototype.processCheckHighlight = function(obj3d, indx) {
       var id = obj3d.mstrId;
       // id = obj3d.eveId;
-      
+
       if (!this.highlight || (this.highlight.id != id)) return true;
-      
+
       // TODO: make precise checks with all combinations
-      return (indx !== this.highlight.indx); 
+      return (indx !== this.highlight.indx);
    }
-   
+
    /** function called by changes from server or by changes from other scenes */
    EveScene.prototype.setElementSelected = function(mstrid, col, indx, from_interactive) {
-      if (!from_interactive) 
+      if (!from_interactive)
          this.selected[mstrid] = { id: mstrid, col: col, indx: indx };
-      
+
       this.drawSpecial(mstrid);
    }
-   
+
    /** Called when processing changes from server or from interactive handler */
    EveScene.prototype.setElementHighlighted = function(mstrid, col, indx, from_interactive) {
-      
+
       // check if other element was highlighted at same time - redraw it
       if (this.highlight && (this.highlight.id != mstrid)) {
          delete this.highlight;
          this.drawSpecial(mstrid);
       }
-      
-      if (!col) 
+
+      if (!col)
          delete this.highlight;
       else
          this.highlight = { id: mstrid, col: col, indx: indx };
-      
+
       this.drawSpecial(mstrid, true);
    }
-   
+
    EveScene.prototype.drawSpecial = function(mstrid, prefer_highlight) {
       var obj3d = this.getObj3D( mstrid, true );
       if (!obj3d || !obj3d.get_ctrl) obj3d = this.getObj3D( mstrid );
@@ -344,8 +355,8 @@
 
       var h1 = this.highlight && (this.highlight.id == mstrid) ? this.highlight : null;
       var h2 = this.selected[mstrid];
-      var ctrl = obj3d.get_ctrl(); 
-      
+      var ctrl = obj3d.get_ctrl();
+
       var did_change = false;
 
       if (ctrl.separateDraw) {
@@ -357,23 +368,23 @@
          var h = prefer_highlight ? (h1 || h2) : (h2 || h1);
          did_change = ctrl.drawSpecial(h ? h.col : null, h ? h.indx : undefined);
       }
-      
-      if (did_change && this.viewer) 
+
+      if (did_change && this.viewer)
          this.viewer.render();
       return did_change;
    }
-   
+
    EveScene.prototype.elementRemoved = function() {
    }
-   
+
    EveScene.prototype.beginChanges = function() {
    }
-   
+
    EveScene.prototype.endChanges = function() {
       if (this.viewer)
          this.viewer.render();
    }
-   
+
    EveScene.prototype.onSceneCreate = function(id)
    {
       this.redrawScene();
@@ -385,7 +396,7 @@
       this[msg.tag](el);
    }
 
-   
+
    JSROOT.EVE.EveScene = EveScene;
 
    return JSROOT;
