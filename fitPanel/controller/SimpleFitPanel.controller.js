82977f8e691b20ae73a70d24035103b781751e70
Sergey Linev <S.Linev@gsi.de>
2019-02-20 14:26:45 +0100

    controller fit panel

--- fitPanel/controller/SimpleFitPanel.controller.js
+++ fitPanel/controller/SimpleFitPanel.controller.js
@@ -10,27 +10,27 @@ sap.ui.define([
    
    "use strict";
 
-   return sap.ui.core.Control.extend("Button", { // call the new Control type "my.ColorBox" and let it inherit from sap.ui.core.Control
+//    return sap.ui.core.Control.extend("Button", { // call the new Control type "my.ColorBox" and let it inherit from sap.ui.core.Control
 
-            // the control API:
-            metadata : {
-               properties : {           // setter and getter are created behind the scenes, incl. data binding and type validation
-                  "color" : {type: "sap.ui.core.CSSColor", defaultValue: "#fff"} // you can give a default value and more
-               }
-            },
+//             // the control API:
+//             metadata : {
+//                properties : {           // setter and getter are created behind the scenes, incl. data binding and type validation
+//                   "color" : {type: "sap.ui.core.CSSColor", defaultValue: "#fff"} // you can give a default value and more
+//                }
+//             },
 
-            // the part creating the HTML:
-            renderer : function(oRm, oControl) { // static function, so use the given "oControl" instance instead of "this" in the renderer function
-               oRm.write("<div"); 
-               oRm.writeControlData(oControl);  // writes the Control ID and enables event handling - important!
-               oRm.addStyle("background-color", oControl.getColor());  // write the color property; UI5 has validated it to be a valid CSS color
-               oRm.writeStyles();
-               oRm.addClass("myColorBox");      // add a CSS class for styles common to all control instances
-               oRm.writeClasses();              // this call writes the above class plus enables support for Square.addStyleClass(...)
-               oRm.write(">"); 
-               oRm.write("</div>"); // no text content to render; close the tag
-            },
-         });
+//             // the part creating the HTML:
+//             renderer : function(oRm, oControl) { // static function, so use the given "oControl" instance instead of "this" in the renderer function
+//                oRm.write("<div"); 
+//                oRm.writeControlData(oControl);  // writes the Control ID and enables event handling - important!
+//                oRm.addStyle("background-color", oControl.getColor());  // write the color property; UI5 has validated it to be a valid CSS color
+//                oRm.writeStyles();
+//                oRm.addClass("myColorBox");      // add a CSS class for styles common to all control instances
+//                oRm.writeClasses();              // this call writes the above class plus enables support for Square.addStyleClass(...)
+//                oRm.write(">"); 
+//                oRm.write("</div>"); // no text content to render; close the tag
+//             },
+//          });
 
    return GuiPanelController.extend("localapp.controller.SimpleFitPanel",{
 
