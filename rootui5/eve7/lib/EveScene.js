e0119306f637e412c3e1f31f4f149c181ef3c402
Sergey Linev <S.Linev@gsi.de>
2019-06-11 09:22:19 +0200

    one more fix in eve7

--- rootui5/eve7/lib/EveScene.js
+++ rootui5/eve7/lib/EveScene.js
@@ -394,7 +394,7 @@ sap.ui.define([
    EveScene.prototype.clearHighlight = function()
    {
       var so = this.mgr.GetElement(this.mgr.global_highlight_id);
-      if (so.prev_sel_list.length) {
+      if (so && so.prev_sel_list && so.prev_sel_list.length) {
          console.log("clearHighlight", this);
          this.mgr.SendMIR({ "mir":        "SelectionCleared()",
                             "fElementId": this.mgr.global_highlight_id,
@@ -408,7 +408,7 @@ sap.ui.define([
    EveScene.prototype.applySelectionOnSceneCreate =  function(selection_id)
    {
       var selection_obj = this.mgr.GetElement(selection_id);
-      if (!selection_obj) return;
+      if (!selection_obj || !selection_obj.prev_sel_list) return;
       var pthis = this;
       selection_obj.prev_sel_list.forEach(function(rec) {
          var prl = pthis.mgr.GetElement(rec.primary);
@@ -449,7 +449,7 @@ sap.ui.define([
       if(sec_idcs === undefined || sec_idcs.length == 0)
       {
          // exit if you try to highlight an object that has already been selected
-         if(estype == THREE.OutlinePass.selection_enum["highlight"] && 
+         if(estype == THREE.OutlinePass.selection_enum["highlight"] &&
             this.viewer.outlinePass.id2obj_map[element_id][this.mgr.global_selection_id] !== undefined
          ) return;
 
